ee9846bcf6b7272e03929d1468e5a42c587fe4a2
Manitriniaina Famenontsoa Rakotonoelina <famenontsoa@outlook.com>
2026-03-23 02:19:09 +0300

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ src/main/java/mg/pizza/wsrest/WsrestApplication.java
@@ -0,0 +1,13 @@
+package mg.pizza.wsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsrestApplication.class, args);
+	}
+
+}
